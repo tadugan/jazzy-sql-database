02b6309b309b223a6f739a91d9c7b7ff4c42444f
Tim Dugan <tadugan@me.com>
2021-06-29 22:42:50 -0500

    Created SQL database and added test data. Updated database.sql with INSERT statements and TABLEs

--- database.sql
+++ database.sql
@@ -1,5 +1,29 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+CREATE TABLE artist (
+	"id" serial PRIMARY KEY,
+	"name" varchar(80),
+	"birthdate" date
 );
+
+CREATE TABLE song (
+	"id" serial PRIMARY KEY,
+	"title" varchar(255),
+	"length" varchar(10),
+	"released" date
+);
+
+INSERT INTO artist ("name", "birthdate")
+	VALUES ('Ella Fitzgerald', '04-25-1917'),
+	('Dave Brubeck', '12-06-1920'),
+	('Miles Davis', '05-26-1926'),
+	('Esperanza Spalding', '10-18-1984');
+	
+INSERT INTO song ("title", "length", "released")
+	VALUES ('Take Five', '5:24', '1959-09-29'),
+	('So What', '9:22', '1959-08-17'),
+	('Black Gold', '5:17', '2012-02-01');
+	
+SELECT *
+FROM artist;
+
+SELECT *
+FROM song;
